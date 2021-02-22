20a30af6305a9dc65658915092c1db7b42dd96fb
augustbrigantino <gus@atollogy.com>
2021-02-22 11:37:33 -0800

    finished pattern finder

--- pattern_finder.py
+++ pattern_finder.py
@@ -1,11 +1,25 @@
-str_input = "aabababbbbaabbaabaaaa"
-pattern = "aab"
+str_input = "miaiscutea ndmiaiss exyandm iasbuttisnice"
+pattern = "butt"
 pat_indxs = []
 currStr = ""
 
 pat_len = len(pattern)
+pat_indx = 0
 
-for x in str_input:
-    currStr += x
+for i in range(len(str_input)):    
+
+    if pattern[pat_indx] == str_input[i]:
+        currStr += str_input[i]
+        pat_indx += 1
+    else:
+        currStr = ""
+        pat_indx = 0
+
+    if currStr == pattern:
+        print("\"" + pattern + "\" found at index: " + str(i - (pat_len - 1)))
+        pat_indx = 0
+        currStr = ""    
+
+
     
 
